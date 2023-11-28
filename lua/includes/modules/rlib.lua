8b48230609bed96a990da1d9205191ef030a502d
ryanm2711 <ryanmaybury123@gmail.com>
2023-11-28 23:39:18 +0000

    Added register addon function

--- lua/includes/modules/rlib.lua
+++ lua/includes/modules/rlib.lua
@@ -2,4 +2,15 @@ AddCSLuaFile()
 require("RLoader")
 module("RLib", package.seeall)
 
+RLib.Addons = RLib.Addons or {}
+
+function RLib:RegisterAddon(name, path)
+    self.Addons[name] = path
+
+    RLoader:Load(path .. "/config", "LUA", function(f) include(f) end, {"config"})
+    RLoader:Load(path, "LUA", function(f) include(f) end)
+
+    hook.Run("RLib.OnAddonRegistered", name, path)
+end
+
 RLoader:Load("rlib", "LUA", function(f) include(f) end)
